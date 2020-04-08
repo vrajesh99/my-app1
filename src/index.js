923aa904f889a00344b2d4b962d6ee33c7ed64bc
Vrajesh Vagadiya <vrajeshvagadiya1999@gmail.com>
2020-04-08 09:07:47 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world! vrajesh!!!');
 ReactDOM.render(element, document.getElementById('root'));
 serviceWorker.unregister();
